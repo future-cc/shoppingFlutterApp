be4e669aaaeb3dcdff54cb2d7e0f8805257bd86f
future <future@gmail.com>
2025-08-22 11:45:45 +0800

    4.2 splash 启动界面

--- lib/pages/system/splash/view.dart
+++ lib/pages/system/splash/view.dart
@@ -9,52 +9,7 @@ class SplashPage extends GetView<SplashController> {
 
   // 主视图
   Widget _buildView() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        // 文字标题
-        GetBuilder<SplashController>(
-          id: "splash_title",
-          builder: (_) {
-            return Center(
-              child: Text(controller.title),
-            );
-          },
-        ),
-        Text(ConfigService.to.version),
-        // 按钮
-        ElevatedButton(
-          onPressed: () {
-            controller.onTap(DateTime.now().microsecondsSinceEpoch);
-          },
-          child: const Text("立刻点击"),
-        ),
-
-        ElevatedButton(
-            onPressed: () {
-              Get.toNamed(RouteNames.systemLogin);
-            },
-            child: const Text("跳转 login")),
-
-        ElevatedButton(
-            onPressed: () {
-              Get.toNamed(RouteNames.stylesStylesIndex);
-            },
-            child: const Text("跳转 style")),
-
-        ElevatedButton(
-            onPressed: () {
-              Get.toNamed(RouteNames.stylesImage);
-            },
-            child: const Text("跳转 imageview")),
-
-        ElevatedButton(
-            onPressed: () {
-              Get.toNamed(RouteNames.stylesInput);
-            },
-            child: const Text("跳转 Input"))
-      ],
-    );
+    return const ImageWidget.img(AssetsImages.splashJpg, fit: BoxFit.fill);
   }
 
   @override
@@ -63,12 +18,7 @@ class SplashPage extends GetView<SplashController> {
       init: SplashController(),
       id: "splash",
       builder: (_) {
-        return Scaffold(
-          appBar: AppBar(title: const Text("splash")),
-          body: SafeArea(
-            child: _buildView(),
-          ),
-        );
+        return _buildView();
       },
     );
   }
