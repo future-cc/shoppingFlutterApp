9bb39e0ed2817ef82f3f962a078a822d47164da9
future <future@gmail.com>
2025-08-30 04:45:39 +0800

    14.5 过滤面板 - 尺寸

--- lib/pages/search/search_filter/controller.dart
+++ lib/pages/search/search_filter/controller.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -8,6 +10,12 @@ class SearchFilterController extends GetxController {
 
   // 价格范围 0~1000
   final List<double> priceRange = [100, 1000];
+
+  // 尺寸列表
+  List<KeyValueModel<AttributeModel>> sizes = [];
+
+  // 选中尺寸列表
+  List<String> sizeKeys = [];
 
   // 全局 key
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
@@ -58,12 +66,32 @@ class SearchFilterController extends GetxController {
     update(["filter_price_range"]);
   }
 
+  // 读取缓存
+  void _loadCache() async {
+    // 尺寸列表
+    String result =
+        Storage().getString(Constants.storageProductsAttributesSizes);
+    sizes = jsonDecode(result).map<KeyValueModel<AttributeModel>>((item) {
+      var arrt = AttributeModel.fromJson(item);
+      return KeyValueModel(key: "${arrt.name}", value: arrt);
+    }).toList();
+  }
+
+  // 尺寸选中
+  void onSizeTap(List<String> keys) {
+    sizeKeys = keys;
+    update(["filter_sizes"]);
+  }
+
+
   void onTap() {}
 
-  // @override
-  // void onInit() {
-  //   super.onInit();
-  // }
+  @override
+  void onInit() {
+    super.onInit();
+    _loadCache();
+  }
+
 
   @override
   void onReady() {
