1851c0e4ca8de8566cd6a319e3383ac37788e54e
future <future@gmail.com>
2025-09-08 23:29:06 +0800

    20.1 修改我的信息页面

---
+++ lib/pages/my/profile_edit/controller.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../../common/index.dart';
+
+class ProfileEditController extends GetxController {
+  ProfileEditController();
+
+  // 表单 form
+  GlobalKey formKey = GlobalKey<FormState>();
+
+  // 输入框控制器
+  TextEditingController firstNameController = TextEditingController();
+  TextEditingController lastNameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController oldPasswordController = TextEditingController();
+  TextEditingController newPasswordController = TextEditingController();
+  TextEditingController confirmNewPasswordController = TextEditingController();
+
+  // 初始数据
+  _initData() {
+    // 用户 profile
+    UserProfileModel profile = UserService.to.profile;
+
+    // 初始值
+    firstNameController.text = profile.firstName ?? "";
+    lastNameController.text = profile.lastName ?? "";
+    emailController.text = profile.email ?? "";
+    update(["profile_edit"]);
+  }
+
+  void onTap() {}
+
+  // @override
+  // void onInit() {
+  //   super.onInit();
+  // }
+
+  @override
+  void onReady() {
+    super.onReady();
+    _initData();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+    firstNameController.dispose();
+    lastNameController.dispose();
+    emailController.dispose();
+    oldPasswordController.dispose();
+    newPasswordController.dispose();
+    confirmNewPasswordController.dispose();
+  }
+
+}
